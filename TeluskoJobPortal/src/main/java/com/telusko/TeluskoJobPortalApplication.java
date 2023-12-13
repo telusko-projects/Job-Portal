5b424adbb6dea388e66661642fe123eed8760af9
GAURAV SHARMA <gsharma1154@gmail.com>
2023-12-13 14:42:34 +0530

    configured Swagger Open Text Api

--- TeluskoJobPortal/src/main/java/com/telusko/TeluskoJobPortalApplication.java
+++ TeluskoJobPortal/src/main/java/com/telusko/TeluskoJobPortalApplication.java
@@ -3,6 +3,11 @@ package com.telusko;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Info;
+import io.swagger.v3.oas.annotations.servers.Server;
+
+@OpenAPIDefinition(info = @Info(title = "JOB PORTAL", version = "1.0.0", description = "JOB PORTAL OPEN API DOCUMENTATION"), servers = @Server(url = "http://localhost:8080", description = "This job portal can be reached through this url."))
 @SpringBootApplication
 public class TeluskoJobPortalApplication {
 
--- TeluskoJobPortal/src/main/java/com/telusko/controller/JobPostController.java
+++ TeluskoJobPortal/src/main/java/com/telusko/controller/JobPostController.java
@@ -13,9 +13,18 @@ import com.telusko.exception.JobPostIdNotFoundException;
 import com.telusko.model.JobPost;
 import com.telusko.service.JobPostService;
 
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
+
 @RestController
+@Tag(name = "JobPostController",description = "to handle incoming requests for job posts")
 public class JobPostController {
 
+	
+	
+	
+	
+	//injecting JobPostService object to use its methods
 	@Autowired
 	private JobPostService jobPostService;
 
@@ -23,6 +32,7 @@ public class JobPostController {
 	
 	
 	// controller method to get all job posts
+	@Operation(summary = "To get all job posts")
 	public ResponseEntity<List<JobPost>> getAllPosts() {
 		return new ResponseEntity<List<JobPost>>(jobPostService.getAllPosts(), HttpStatus.OK);
 
@@ -31,50 +41,72 @@ public class JobPostController {
 	
 	
 	
+
+	// ******************************************************************
+
 	
 	
-	//******************************************************************
 	
 	
 	
-	//controller method to get a single job post by Id
-	public ResponseEntity<JobPost> getJobPostById(@PathVariable int postId) throws JobPostIdNotFoundException{
-		return new ResponseEntity<JobPost>(jobPostService.getJobPostById(postId),HttpStatus.OK);
-		
+	// controller method to get a single job post by Id
+	@Operation(summary = "To get job post by id")
+	public ResponseEntity<JobPost> getJobPostById(@PathVariable int postId) throws JobPostIdNotFoundException {
+		return new ResponseEntity<JobPost>(jobPostService.getJobPostById(postId), HttpStatus.OK);
+
+	}
+
+	
+	
+	
+	
+	// ******************************************************************
+
+	
+	
+	
+	
+	// controller method to add a job Post
+	@Operation(summary = "To add a job post")
+	public ResponseEntity<JobPost> addAJobPost(@RequestBody JobPost post) {
+		return new ResponseEntity<JobPost>(jobPostService.addNewJobPost(post), HttpStatus.CREATED);
+
 	}
 	
 	
 	
 	
-	//******************************************************************
 	
-	
-	//controller method to add a job Post
-	public ResponseEntity<JobPost> addAJobPost(@RequestBody JobPost post){
-		return new ResponseEntity<JobPost>(jobPostService.addNewJobPost(post),HttpStatus.CREATED);
-		
-	}
+
+	// ******************************************************************
+
 	
 	
 	
 	
-	//******************************************************************
-	
-	
-	//controller method to delete a job post
-	public ResponseEntity<String> deleteJobPost(@PathVariable int postId) throws JobPostIdNotFoundException{
-		return new ResponseEntity<String>(jobPostService.deleteJobPostById(postId),HttpStatus.OK);
-		
+	// controller method to delete a job post
+	@Operation(summary = "To delete all job post by id")
+	public ResponseEntity<String> deleteJobPost(@PathVariable int postId) throws JobPostIdNotFoundException {
+		return new ResponseEntity<String>(jobPostService.deleteJobPostById(postId), HttpStatus.OK);
+
 	}
 	
 	
 	
 	
 	
-	//******************************************************************
+
+	// ******************************************************************
+
 	
-	//controller method to update a jobPost
-	public ResponseEntity<JobPost> updateJobPostById(@PathVariable int postId,@RequestBody JobPost post) throws JobPostIdNotFoundException{
-		return new ResponseEntity<JobPost>(jobPostService.updateJobPost(postId, post),HttpStatus.CREATED);
+	
+	
+	
+	
+	// controller method to update a jobPost
+	@Operation(summary = "To update job post by id")
+	public ResponseEntity<JobPost> updateJobPostById(@PathVariable int postId, @RequestBody JobPost post)
+			throws JobPostIdNotFoundException {
+		return new ResponseEntity<JobPost>(jobPostService.updateJobPost(postId, post), HttpStatus.CREATED);
 	}
 }
