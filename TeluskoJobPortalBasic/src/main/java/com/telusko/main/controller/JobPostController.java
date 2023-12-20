b0d25704c196dedde56e763cabf0ebbabf40c889
GAURAV SHARMA <gsharma1154@gmail.com>
2023-12-21 01:06:18 +0530

    only getAll JobPost API

--- TeluskoJobPortalBasic/src/main/java/com/telusko/main/controller/JobPostController.java
+++ TeluskoJobPortalBasic/src/main/java/com/telusko/main/controller/JobPostController.java
@@ -4,12 +4,7 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.PutMapping;
-import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -36,60 +31,15 @@ public class JobPostController {
 	
 	
 	
-	//************************************************************
-	
-	
-	//controller method to get a Job Post By Id
-	@CrossOrigin
-	@GetMapping("/getJobPostById/{id}")
-	public JobPost getJobPostById(@PathVariable int id) {
-		return jobPostService.returnJobPostById(id);
-	}
-	
-	
-	
-	
-	//************************************************************
-	
-	
-	
-	//controller method to add a job post
-	@CrossOrigin
-	@PostMapping("/addJobPost")
-	public JobPost addJobPost(@RequestBody JobPost jobPost) {
-		return jobPostService.addJobPost(jobPost);
-	}
-	
-	
-	
-	
-	//************************************************************
+
 	
 	
 	
 	
 	
-	//controller method to update a job post by id
-	@CrossOrigin
-	@PutMapping("/updateJobPostById/{id}")
-	public JobPost updateJobPost(@PathVariable int id,@RequestBody JobPost jobPost) {
-		return jobPostService.updateJobPostById(id, jobPost);
-	}
 	
 	
 	
 	
-	
-	//************************************************************
-	
-	
-	
-//controller method to save a JobPost
-	@DeleteMapping("/deleteJobPostById/{id}")
-	@CrossOrigin
-	public String deleteJobPostById(@PathVariable int id) {
-		return jobPostService.deleteJobPostById(id);
-		
-	}
-	
+
 }
